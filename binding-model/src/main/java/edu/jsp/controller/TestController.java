307432031922833a9562b3399f89569e41cdf0fb
Noor1-git <shaikh7768930208@gmail.com>
2023-09-25 11:42:24 +0530

    program on binding data using Model inteface

---
+++ binding-model/src/main/java/edu/jsp/controller/TestController.java
@@ -0,0 +1,16 @@
+package edu.jsp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("/getmessage")
+	public String getMessage(Model model) {
+		model.addAttribute("myName", "Noor");
+		
+		return "message.jsp";
+	}
+}
